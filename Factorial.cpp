b13bd3fea1cce569322316b389fc7c4210294204
Nick <nick.tiumentcev2017@yandex.ru>
2023-03-04 15:22:22 +0300

    Add project files.

---
+++ Factorial.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int iFactorial(int);
+
+int main()
+{
+	int iNumber = 0;
+
+	cout << iFactorial(iNumber) << endl;
+
+	return 0;
+}
+
+int iFactorial(int number)
+{
+	int result = number;
+	number--;
+
+	if (number > 0)
+	{
+		result *= iFactorial(number);
+	}
+	else
+	{
+		result = 1;
+	}
+
+	return result;
+}
+
